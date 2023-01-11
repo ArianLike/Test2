bd46caac4fad26d322fe48f01733fc5f60d22e62
Arian Leik <arian.leik@gmail.com>
2023-01-11 11:16:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Test2;
+using System.ComponentModel;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        Console.WriteLine(calculator.Add(5, 7));
+
+    }
+}
